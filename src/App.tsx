82fb404cf51a99664b9d8935db8dbd8a7b5d7fad
vuvuvo0303 <levanhoangvu2003@gmail.com>
2025-02-03 20:58:05 +0700

    install lib

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-4xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
