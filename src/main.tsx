a5a00a13efb935b80f2ebefc1c9af09d85d62d63
vuvuvo0303 <levanhoangvu2003@gmail.com>
2025-02-03 21:39:35 +0700

    modify login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+      <Sonner />
+    </BrowserRouter>
+  </StrictMode>
+);
--- src/pages/auth/login-page.tsx
+++ src/pages/auth/login-page.tsx
@@ -7,17 +7,17 @@ import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { z } from "zod";
 import Wooly_logo from "@/assets/images/wooly_logo.png";
-import googleLogo from "@/assets/images/google.webp";
-import { Checkbox } from "@/components/ui/checkbox";
-import { useState } from "react";
-import loginBackground from "@/assets/images/login-background.jpg";
+
+// import { Checkbox } from "@/components/ui/checkbox";
+// import { useState } from "react";
+import loginbg from "@/assets/images/loginbg.png";
 const formSchema = z.object({
   email: z.string().email("Email không hợp lệ"),
   password: z.string().min(5, "Mật khẩu phải chứa ít nhất 5 ký tự"),
 });
 const LoginPage = () => {
   const navigate = useNavigate();
-  const [rememberMe, setRememberMe] = useState(false);
+  // const [rememberMe, setRememberMe] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -26,11 +26,11 @@ const LoginPage = () => {
     },
   });
   const handleSubmit = () => {
-    console.log("Remember me: " + rememberMe);
+    // console.log("Remember me: " + rememberMe);
     navigate("/");
   };
   return (
-    <div className="grid grid-cols-12" style={{ backgroundImage: `url(${loginBackground})`, backgroundSize: "cover" }}>
+    <div className="grid grid-cols-12" style={{ backgroundImage: `url(${loginbg})`, backgroundSize: "cover" }}>
       <div className="col-span-6 h-screen overflow-auto rounded-e shadow-md bg-white">
         <div className="bg-white min-h-screen py-16 px-24">
           <Form {...form}>
@@ -69,7 +69,7 @@ const LoginPage = () => {
                 )}
               />
               <div className="flex justify-between mt-5">
-                <div className="items-top flex space-x-2">
+                {/* <div className="items-top flex space-x-2">
                   <Checkbox
                     id="remember-me"
                     checked={rememberMe}
@@ -83,28 +83,21 @@ const LoginPage = () => {
                   >
                     Ghi nhớ tài khoản này
                   </label>
-                </div>
+                </div> */}
                 <Link to="/recover-password" className="text-sm text-orange-500">
                   Quên mật khẩu?
                 </Link>
               </div>
               <Button
-                className="mt-5 bg-orange-500 hover:bg-orange-600 text-white font-bold p-6 focus:outline-none focus:shadow-outline w-full"
+                className="mt-5 bg-blue-500 hover:bg-orange-600 text-white font-bold p-6 focus:outline-none focus:shadow-outline w-full"
                 type="submit"
               >
                 Đăng Nhập
               </Button>
             </form>
           </Form>
-          <div className="flex justify-between gap-5 items-center my-5">
-            <hr className="border flex-1" />
-            <p className="uppercase text-sm">Hoặc</p>
-            <hr className="border flex-1" />
-          </div>
-          <Button className="w-full p-6" variant={"outline"}>
-            <img src={googleLogo} alt="" width={30} />
-            Đăng nhập bằng tài khoản Google
-          </Button>
+        
+      
         </div>
       </div>
     </div>
