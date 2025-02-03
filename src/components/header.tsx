0a9e2a5bd8855838d02d22edcf9e12b9699cf47a
vuvuvo0303 <levanhoangvu2003@gmail.com>
2025-02-03 22:43:55 +0700

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,15 +8,7 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "./ui/sidebar";
-const Header = ({
-  title,
-  href,
-  currentPage,
-}: {
-  title: string;
-  href: string;
-  currentPage: string;
-}) => {
+const Header = ({ title, href, currentPage }: { title: string; href: string; currentPage: string }) => {
   return (
     <header className="border flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4">
